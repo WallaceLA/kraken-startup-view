b6fe48ca75d67d90b845b16200f4749aa3fa6543
Julio Avila <julioavila@Julios-Mac.local>
2020-07-19 07:32:19 -0700

    fineshed Deal Actions configuration

--- client-ios/Parken/Pages/Main/Home/HomeViewController.swift
+++ client-ios/Parken/Pages/Main/Home/HomeViewController.swift
@@ -6,73 +6,18 @@ class HomeViewController: UIViewController {
     
     @IBOutlet var mapView: MapViewLite!
     
-     var animator: Jelly.Animator?
+    var dealActionsAnimator: Jelly.Animator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         
-        let uiConfiguration = PresentationUIConfiguration(
-            cornerRadius: 10,
-            backgroundStyle: .dimmed(alpha: 0.0),
-            isTapBackgroundToDismissEnabled: false)
-        
-        let size = PresentationSize(
-            width: .custom(value: (self.view.frame.width * 0.8)),
-            height: .custom(value: (self.view.frame.height * 0.4)))
-        
-        let alignment = PresentationAlignment(vertical: .bottom, horizontal: .center)
-        
-        let marginGuards = UIEdgeInsets(top: 40, left: 16, bottom: 40, right: 16)
-        
-        let timing = PresentationTiming(duration: .normal, presentationCurve: .linear, dismissCurve: .linear)
-
-        let interactionConfiguration = InteractionConfiguration(
-            presentingViewController: self,
-            completionThreshold: 0.5,
-            dragMode: .edge)
-        
-        /*
-        let slidePresentation = SlidePresentation(
-            direction: .bottom,
-            size: .halfscreen
-        )
-        */
-        
-        let slidePresentation = CoverPresentation(
-            directionShow: .top,
-            directionDismiss: .bottom,
-            uiConfiguration: uiConfiguration,
-            size: size,
-            alignment: alignment,
-            marginGuards: marginGuards,
-            timing: timing,
-            spring: .none,
-            interactionConfiguration: interactionConfiguration)
-        
-        
-        //                   storyboard!.instantiateViewController(withIdentifier: "DealActionsViewController")
-        //let viewController = DealActionsViewController()
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "DealActions") as! DealActionsViewController
-        
-        
-        animator = Animator(presentation: slidePresentation)
-        animator?.prepare(presentedViewController: viewController)
-        present(viewController, animated: true, completion: nil)
-        
-        if true {
-            viewController.interactionAction = { [weak self] in
-                   let newGuards = UIEdgeInsets(top: 70, left: 8, bottom: 0, right: 8)
-                 try! self?.animator?.updateMarginGuards(marginGuards: newGuards, duration: .medium)
-            }
-        }
-       
-        mapView.mapScene.loadScene(mapStyle: .normalDay, callback: onLoadScene)
+        onLoadCustomPresentation()
+        mapView.mapScene.loadScene(mapStyle: .normalDay, callback: onLoadMap)
     }
     
-    func onLoadScene(errorCode: MapSceneLite.ErrorCode?) {
+    func onLoadMap(errorCode: MapSceneLite.ErrorCode?) {
         if let error = errorCode {
             print("Error: Map scene not loaded, \(error)")
         } else {
@@ -80,6 +25,58 @@ class HomeViewController: UIViewController {
             mapView.camera.setTarget(GeoCoordinates(latitude: 52.518043, longitude: 13.405991))
             mapView.camera.setZoomLevel(13)
         }
+    }
+    
+    func onLoadCustomPresentation() {
+         let uiConfiguration = PresentationUIConfiguration(
+             cornerRadius: 10,
+             backgroundStyle: .dimmed(alpha: 0.0),
+             isTapBackgroundToDismissEnabled: false,
+             corners: [.layerMaxXMinYCorner,.layerMinXMinYCorner])
+         
+         let size = PresentationSize(
+             width: .fullscreen,
+             height: .custom(value: (self.view.frame.height * 0.2)))
+         
+         let alignment = PresentationAlignment(vertical: .bottom, horizontal: .center)
+         
+         let marginGuards = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+         
+         let timing = PresentationTiming(duration: .normal, presentationCurve: .linear, dismissCurve: .linear)
+
+         let interactionConfiguration = InteractionConfiguration(
+             presentingViewController: self,
+             completionThreshold: 0.5,
+             dragMode: .edge)
+         
+        
+         let dealActionsPresentation = CoverPresentation(
+             directionShow: .bottom,
+             directionDismiss: .bottom,
+             uiConfiguration: uiConfiguration,
+             size: size,
+             alignment: alignment,
+             marginGuards: marginGuards,
+             timing: timing,
+             spring: .none,
+             interactionConfiguration: interactionConfiguration)
+        
+         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+         let viewController = storyboard.instantiateViewController(withIdentifier: "DealActions") as! DealActionsViewController
+         
+         dealActionsAnimator = Animator(presentation: dealActionsPresentation)
+         dealActionsAnimator?.prepare(presentedViewController: viewController)
+         present(viewController, animated: true, completion: nil)
+         
+         if false {
+             viewController.interactionAction = { [weak self] in
+                let newSize = PresentationSize(
+                    width: .fullscreen,
+                    height: .custom(value: (self!.view.frame.height * 0.8)))
+                try! self?.dealActionsAnimator?.updateSize(presentationSize: newSize, duration: .medium)
+             }
+         }
+        
     }
     
     override func didReceiveMemoryWarning() {
